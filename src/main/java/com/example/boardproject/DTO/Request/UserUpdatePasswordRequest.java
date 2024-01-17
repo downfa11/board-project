2c6dea01ccff526d39b2fcef7e5eb581cbfa1c10
downfa11 <downfa11@naver.com>
2024-01-18 03:49:22 +0900

    Revert "Revert "User Login implement""
    This reverts commit def3dd1417654cceba84ba20fbe488c239effe8d.

---
+++ src/main/java/com/example/boardproject/DTO/Request/UserUpdatePasswordRequest.java
@@ -0,0 +1,13 @@
+package com.example.boardproject.DTO.Request;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserUpdatePasswordRequest {
+    @NonNull
+    private String beforePassword;
+    @NonNull
+    private String afterPassword;
+}
